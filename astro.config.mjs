324407ad1840d51b42bd4d2d37fa0edd232b4f62
1nCharles <2855057900@qq.com>
2025-07-21 22:54:52 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://1ncharles.github.io/",
-	base: "1nCharles.github.io",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
